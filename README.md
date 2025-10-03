d1350e7a6810ff9cbbb40858d2ad0a850b8c47bc
OttoHag <geirolar@hotmail.com>
2025-10-03 05:34:30 +0200

    Oppdatert prosjekt for Python 3.6 kompatibilitet

--- README.md
+++ README.md
@@ -1,3 +1,24 @@
 # Welcome to Microblog!
 
 This is an example application featured in my [Flask Mega-Tutorial](https://blog.miguelgrinberg.com/post/the-flask-mega-tutorial-part-i-hello-world). See the tutorial for instructions on how to work with it.
+
+
+# Microblog
+
+A simple microblogging web application built with Flask. This project is designed to help you learn how to build a web app with user authentication, database integration, and basic CRUD functionality.
+
+## 🚀 Technologies Used
+
+- Flask
+- Flask-WTF
+- Flask-Migrate
+- Flask-SQLAlchemy
+- Bootstrap (via templates)
+
+## 📦 Installation
+
+1. Clone the repository:
+   ```bash
+   git clone https://github.com/OttoHag/microblog.git
+   cd microblog
+   ```
--- app/models.py
+++ app/models.py
@@ -4,31 +4,24 @@ import sqlalchemy as sa
 import sqlalchemy.orm as so
 from app import db
 
-
 class User(db.Model):
-    id: so.Mapped[int] = so.mapped_column(primary_key=True)
-    username: so.Mapped[str] = so.mapped_column(sa.String(64), index=True,
-                                                unique=True)
-    email: so.Mapped[str] = so.mapped_column(sa.String(120), index=True,
-                                             unique=True)
-    password_hash: so.Mapped[Optional[str]] = so.mapped_column(sa.String(256))
-
-    posts: so.WriteOnlyMapped['Post'] = so.relationship(
-        back_populates='author')
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
 
 
-class Post(db.Model):
-    id: so.Mapped[int] = so.mapped_column(primary_key=True)
-    body: so.Mapped[str] = so.mapped_column(sa.String(140))
-    timestamp: so.Mapped[datetime] = so.mapped_column(
-        index=True, default=lambda: datetime.now(timezone.utc))
-    user_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey(User.id),
-                                               index=True)
 
-    author: so.Mapped[User] = so.relationship(back_populates='posts')
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), index=True)
+
+    author = db.relationship('User', backref=db.backref('posts', lazy='dynamic'))
 
     def __repr__(self):
         return '<Post {}>'.format(self.body)
