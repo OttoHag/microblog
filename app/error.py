6e9a990c575739a9144f70274361ca255bce0f9e
OttoHag <geirolar@hotmail.com>
2025-10-24 17:56:21 +0200

    Automatisk commit fra bash-script

--- app/error.py
+++ app/error.py
@@ -1,11 +1,14 @@
-from flask import render_template
-from app import app, db
+from flask import Blueprint, render_template
+from app import db
+from app.routes import bp  # eller lag en egen blueprint for errors
 
-@app.errorhandler(404)
+bp_error = Blueprint('errors', __name__)
+
+@bp.app.errorhandler(404)
 def not_found_error(error):
     return render_template('404.html'), 404
 
-@app.errorhandler(500)
+@bp.app.errorhandler(500)
 def internal_error(error):
     db.session.rollback()
     return render_template('500.html'), 500
--- config.py
+++ config.py
@@ -12,9 +12,9 @@ class Config:
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False  # Slår av unødvendig overhead
 
-    MAIL_SERVER = os.environ.get('MAIL_SERVER')
-    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
-    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD') or None # Tillat tomt passord
-    ADMINS = ['geirolar@hotmail.com']  # Liste over admin-e-poster
+MAIL_SERVER = os.environ.get('MAIL_SERVER') or 'smtp.office365.com'
+MAIL_PORT = int(os.environ.get('MAIL_PORT') or 587)
+MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+MAIL_USERNAME = os.environ.get('MAIL_USERNAME') or 'geirolar@hotmail.com'
+MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD') or None
+ADMINS = ['geirolar@hotmail.com']
